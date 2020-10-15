e62291824007b4d7b9d035a053a02af6e1cdfce7
katarzyna <jeznach_katarzyna@yahoo.com>
2020-10-15 12:39:00 +0200

    wersja 2

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -24,24 +26,26 @@ public class TaskManager {
         printOptions(OPTIONS);
 loadDataToTab(FILE_NAME);
 
+
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            switch (input) {
-                case "exit":
-                    saveTabToFile(FILE_NAME, tasks);
+            switch (input) { case "exit":
+                    saveFile(FILE_NAME, tasks);
                     System.out.println(RED + "Bye, bye."+ RESET);
-                    System.exit(0);
+                   System.exit(0);
                     break;
                 case "add":
                     addTask();
                     break;
                 case "remove":
-                    removeTask(tasks, getTheNumber());
+                    removeTask(tasks,getTheNumber());
                     System.out.println("Value was successfully deleted.");
+                    saveFile(FILE_NAME, tasks);
                     break;
                 case "list":
-                    printTab(tasks);
+                    printTasks(tasks);
                     break;
                 default:
                     System.out.println("Please select a correct option.");
@@ -49,65 +53,58 @@ loadDataToTab(FILE_NAME);
             printOptions(OPTIONS);
         }
 
-
-
-
-
-
         }
 
-        public static void printOptions (String[]tab){
+        public static void printOptions (String [] tab){
 
             System.out.println(BLUE + "Please select an option: " + RESET);
-            for (String option : tab) {
-                System.out.println(option);
+            for (String OPTIONS : OPTIONS) {
+                System.out.println(OPTIONS);
 
             }
         }
 
-        public static String[][] loadDataToTab (String fileName){
+        public static String[][] loadDataToTab (String FILE_NAME){
 
-            Path dir = Paths.get(fileName);
-
+            Path dir = Paths.get(FILE_NAME);
             if (!Files.exists(dir)) {
-
                 System.out.println("File not exist.");
-
                 System.exit(0);
-
             }
-
-
-            String[][] tab = null;
 
             try {
                 List<String> strings = Files.readAllLines(dir);
-                tab = new String[strings.size()][strings.get(0).split(",").length];
+                tasks = new String[strings.size()][strings.get(0).split(",").length];
+
                 for (int i = 0; i < strings.size(); i++) {
                     String[] split = strings.get(i).split(",");
                     for (int j = 0; j < split.length; j++) {
-                        tab[i][j] = split[j];
+                        tasks[i][j] = split[j];
                     }
-                }
+                   }
+
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            return tab;
-        }
 
-        public static void printTab (String[][]tab){
-            for (int i = 0; i < tab.length; i++) {
+            }
+            return tasks;
+        }
+        public static void printTasks (String[][]tasks){
+
+
+            for (int i = 0; i < tasks.length; i++) {
+
                 System.out.print(i + " : ");
-                for (int j = 0; j < tab[i].length; j++) {
-                    System.out.print(tab[i][j] + " ");
+
+                for (int j = 0; j < tasks[i].length; j++) {
+                    System.out.print(tasks[i][j] + " ");
                 }
                 System.out.println();
             }
         }
 
 
-
-        private static void addTask () {
+    private static void addTask () {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please add task description");
             String description = scanner.nextLine();
@@ -139,21 +136,36 @@ loadDataToTab(FILE_NAME);
             }
             return Integer.parseInt(n);
         }
-        private static void removeTask (String[][]tab,int index){
-            try {
-                if (index < tab.length) {
-                    tasks = ArrayUtils.remove(tab, index);
-                }
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                System.out.println("Element not exist in tab");
-            }
-        }
-        public static void saveTabToFile (String fileName, String[][]tab){
+        private static void removeTask (String[][]tasks,int n) {
 
-            Path dir = Paths.get(fileName);
+
+
+
+                if (n < tasks.length) {
+                    for (int i = 0; i < tasks.length; i++) {
+                        if (tasks[i].equals(n)) {
+
+                            try {
+                                tasks = ArrayUtils.removeElement(tasks, n);
+
+
+                            } catch (ArrayIndexOutOfBoundsException ex) {
+                                System.out.println("Element not exist in file");
+                            }
+                        }
+
+                    }
+                }
+            }
+
+
+
+        public static void saveFile (String FILE_NAME, String[][]tasks){
+
+            Path dir = Paths.get(FILE_NAME);
             String[] lines = new String[tasks.length];
-            for (int i = 0; i < tab.length; i++) {
-                lines[i] = String.join(",", tab[i]);
+            for (int i = 0; i < tasks.length; i++) {
+                lines[i] = String.join(",", tasks[i]);
             }
             try {
                 Files.write(dir, Arrays.asList(lines));
